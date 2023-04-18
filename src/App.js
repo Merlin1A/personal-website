3a39206216ace6f897ff14ba640af7da2d8074a7
Jesse Brookins <jessebrookins@protonmail.com>
2023-04-18 17:56:51 -0400

    Add template

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          This website is under construction. Please check back later.
+        </p>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Landing/Landing.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Landing.css';
+
+const Landing = () => {
+  return (
+    <div className="landing-page">
+      <h1>Welcome to My Website</h1>
+      <p>Currently under development. Please check back soon.</p>
+    </div>
+  );
+};
+
+export default Landing;
